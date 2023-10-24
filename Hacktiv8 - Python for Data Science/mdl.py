2c83d1a4062a4fc05c15fbb2feb4f05d69d1fa08
Nur Ishmawati <142519665+nurishmawati@users.noreply.github.com>
2023-10-24 12:03:47 +0700

    .ipynb

---
+++ Hacktiv8 - Python for Data Science/mdl.py
@@ -0,0 +1,13 @@
+p = "Hacktiv8 Python"
+a = [100, 200, 300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+    
+class Foo():
+    pass
+
+if (__name__ == '__main__'):
+print(p)
+print(a)
+foo([1, 2, 3])
